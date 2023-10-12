11f4d6b92f8864ae3c75332092ac9a25dc1f21ce
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-10-12 12:49:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CA23101205
+{
+    class Program
+    {
+        static void Main()
+        {
+            var requiests = new List<Request>();
+            using var sr = new StreamReader(@"..\..\..\src\NASAlog.txt", Encoding.UTF8);
+            while (!sr.EndOfStream) requiests.Add(new(sr.ReadLine()));
+
+            Console.WriteLine($"5. feladat: Kérések száma: {requiests.Count}");
+
+            var f6 = requiests.Sum(r => r.ByteSize);
+            Console.WriteLine($"6. feladat: Válaszok összmérete: {f6} byte");
+
+            var f8 = (float)requiests.Count(r => r.HaveDomain) / requiests.Count * 100f;
+            Console.WriteLine($"8. feladat: Domaines kérések: {f8:0.00}%");
+
+            var f9 = requiests
+                .GroupBy(r => r.Status)
+                .ToDictionary(k => k.Key, v => v.Count());
+            Console.WriteLine("9. feladat: Statisztika:");
+            foreach (var kvp in f9) Console.WriteLine($"\t{kvp.Key}: {kvp.Value,4} db");
+
+        }
+    }
+}
---
+++ Request.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA23101205
+{
+    class Request
+    {
+        public string Address { get; set; }
+        public bool HaveDomain => !char.IsDigit(Address.Last());
+        public DateTime TimeStamp { get; set; }
+        public string File { get; set; }
+        public int Status { get; set; }
+        public int ByteSize { get; set; }
+
+        public Request(string row)
+        {
+            var splts = row.Split('*');
+            Address = splts[0];
+            var d = splts[1].Split('/', ':');
+            TimeStamp = DateTime.Parse($"{d[2]}-{d[1]}-{d[0]} {d[3]}:{d[4]}:{d[5]}");
+            File = splts[2];
+            var srv = splts[3].Split(' ');
+            Status = int.Parse(srv[0]);
+            ByteSize = srv[1] == "-" ? 0 : int.Parse(srv[1]);
+        }
+    }
+}
